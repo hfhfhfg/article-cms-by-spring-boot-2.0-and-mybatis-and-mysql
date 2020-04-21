fd99a313881b2b2e2d305a75599d6d94d744e816
hyeonuk <hfhfhfg5@gmail.com>
2020-04-21 18:42:52 +0900

    ArticleDao 생성 및 적용

--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -3,24 +3,26 @@ package com.sbs.starter.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sbs.starter.dao.ArticleDao;
 import com.sbs.starter.dto.Article;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
+	@Autowired
+	ArticleDao articleDao;
+	//이제 AtricleService는 데이터 관련해서는 모두 Dao에 위임
 	public List<Article> getList() {
-		//중간확인용 임시데이터 추가
-		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1","내용1");
-		Article article2 = new Article(2, "2019-08-20 12:12:12", "제목2","내용2");
-		Article article3 = new Article(3, "2019-08-20 12:12:12", "제목3","내용3");
+
 		
-		List<Article> list = new ArrayList<>();
 		
-		list.add(article3);
-		list.add(article2);
-		list.add(article1);
 		
-		return list;
+		
+		
+		
+		
+		return articleDao.getList();
 	}
 }
