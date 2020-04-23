86a9c34d70946a205f378f8e36f3c4b11fb730ff
hyeonuk <hfhfhfg5@gmail.com>
2020-04-23 22:00:15 +0900

    CUtile 도입 useGeneratedKeys 옵션으로 문제 해결

--- src/main/java/com/sbs/starter/util/CUtil.java
+++ src/main/java/com/sbs/starter/util/CUtil.java
@@ -29,6 +29,7 @@ public class CUtil {
 		//System.out.println("오브젝트 확인 : "+ object +"\n 오브젝트의 클래스 확인 : "+ object.getClass());
 		return -1;//(Long)object; // 위에 조건문에 걸리지않으면 -1로 이상현상을 리턴하려 했으나 실패하여 통째로 리턴
 							//  추측 : db에서 넘어올때 형변환을 하지 않았다? 혹은 하였으나 캐치하지 못한다?
+							// dao.xml(myBatis설정) 의 userGeneratedKeys, keyProperty 옵션으로 문제 해결
 	}
 
 }
