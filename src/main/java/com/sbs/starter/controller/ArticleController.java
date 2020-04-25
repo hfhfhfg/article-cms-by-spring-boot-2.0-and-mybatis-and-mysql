03a0ed39f27dfbda4a2ec7fd558ad7c5eaa70eda
hyeonuk <hfhfhfg5@gmail.com>
2020-04-25 21:19:32 +0900

    게시물 추가 후 상세페이졸 이동되도록 처리(아직 상세페이지 구현x)

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -50,7 +50,17 @@ public class ArticleController {
 		
 		long newId = articleService.add(param);
 		
-		return newId + "번 게시물이 추가 되었습니다.";
+		String msg = newId + "번 게시물이 추가 되었습니다.";
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("alert('" + msg + "');");
+		sb.append("location.replace('./detail?id=" + newId + "');");
+		
+		sb.insert(0,"<script>");
+		sb.append("</script>");
+		
+		return sb.toString();
 		
 	}
 }
